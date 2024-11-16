7dd2bb4f931cf9b7a1e210e2715205e552402002
Diamond Buchanan <winterdiamon@hotmail.com>
2024-11-16 00:54:14 -0500

    Incident

---
+++ Database/Incident.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Incident] (
+    [IncidentID]     INT         IDENTITY (1, 1) NOT NULL,
+    [IncidentParish] VARCHAR (8) NULL,
+    [IncidentTime]   TIME (7)    NULL,
+    PRIMARY KEY CLUSTERED ([IncidentID] ASC)
+);
+
