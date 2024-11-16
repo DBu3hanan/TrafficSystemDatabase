f684d71496f26a89bab962ab09f9c16fced4dc03
Diamond Buchanan <winterdiamon@hotmail.com>
2024-11-16 00:52:17 -0500

    Add project files.

---
+++ Database/Driver.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Driver] (
+    [DriverID]        INT           IDENTITY (1, 1) NOT NULL,
+    [DriverLastName]  VARCHAR (150) NULL,
+    [DriverFirstName] VARCHAR (150) NULL,
+    [DriverDOB]       INT           NULL,
+    [DriverParish]    VARCHAR (150) NULL,
+    PRIMARY KEY CLUSTERED ([DriverID] ASC)
+);
+
